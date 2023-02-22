4774ce5f31cbc96ae04a9d2c7d0e1e28e188c3d9
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-02-22 10:03:15 -0300

    #003 - Data Annotations

--- CardapioWEB_Demo/Models/Categoria.cs
+++ CardapioWEB_Demo/Models/Categoria.cs
@@ -1,9 +1,20 @@
-﻿namespace CardapioWEB_Demo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWEB_Demo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int Id { get; set; }
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Nome")]
         public string Descricao { get; set; }
 
         //Relacionamento de 1 pra muitos N
--- CardapioWEB_Demo/Models/Lanche.cs
+++ CardapioWEB_Demo/Models/Lanche.cs
@@ -1,15 +1,41 @@
-﻿namespace CardapioWEB_Demo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWEB_Demo.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int lancheID { get; set; }
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} caracteres")]
+        [Display(Name = "Nome do lanche")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "A descrição do lanche deve ser informado")]
+        [Display(Name = "Descrição do lanche")]
+        [MinLength(20, ErrorMessage = " Descrição deve tetr no minimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição deve ter no máximo {1} caracteres")]
         public string DescricaoCurta { get; set; }
+        [Required(ErrorMessage = "A descrição do lanche deve ser informado")]
+        [Display(Name = "Descrição do lanche")]
+        [MinLength(20, ErrorMessage = " Descrição deve tetr no minimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição deve ter no máximo {1} caracteres")]
         public string DescricaoDetalhada { get; set; }
+        [Required(ErrorMessage = "Informe o preço do lanche")]
+        [Display(Name = "Preco")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 999.99, ErrorMessage = "O preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImageUrl { get; set; }
+        [Display(Name = "Caminho Imagem Miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; set; }
+        [Display(Name = "Preferido?")]
         public bool IsLanchePreferido { get; set; }
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
 
         //Definir relacionamento entre lanche e categoria
