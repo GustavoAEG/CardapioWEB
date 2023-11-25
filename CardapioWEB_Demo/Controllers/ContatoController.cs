7e2b76fbb3fef4c0b3d3de1423a5b4ee8da60637
Gustavo <p.gustavo.goncalves@viveo.com.br>
2023-11-25 17:36:39 -0300

    Aba Contato e imagens

---
+++ CardapioWEB_Demo/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CardapioWEB_Demo.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CardapioWEB_Demo/TagHelper/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CardapioWEB_Demo.TagHelpers
+{
+    // Renomeie o namespace para evitar colisões
+    public class EmailTagHelper : Microsoft.AspNetCore.Razor.TagHelpers.TagHelper
+    {
+        public string Endereco { get; set; }
+        public string Conteudo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Endereco);
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
