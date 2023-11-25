54f0c19120938ea7bed9bb7121844d1552c199c3
Gustavo <p.gustavo.goncalves@viveo.com.br>
2023-11-25 19:23:27 -0300

    Roteamento_2

--- CardapioWEB_Demo/Controllers/LancheController.cs
+++ CardapioWEB_Demo/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using CardapioWEB_Demo.Repositories.Interfaces;
+﻿using CardapioWEB_Demo.Models;
+using CardapioWEB_Demo.Repositories.Interfaces;
 using CardapioWEB_Demo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,36 +8,45 @@ namespace CardapioWEB_Demo.Controllers
     public class LancheController : Controller
     {
         private readonly ILancheRepository _lancheRepository;
-
         public LancheController(ILancheRepository lancheRepository)
         {
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
 
-            //var lanches = _lancheRepository.Lanches;
-            //var totalLanches = lanches.Count();
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.lancheID);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                {
+                    lanches = _lancheRepository.Lanches
+                        .Where(l => l.Categoria.Nome.Equals("NORMAL"))
+                        .OrderBy(l => l.Nome);
+                }
+                else
+                {
+                    lanches = _lancheRepository.Lanches
+                       .Where(l => l.Categoria.Nome.Equals("NATURAL"))
+                       .OrderBy(l => l.Nome);
+                }
+                categoriaAtual = categoria;
+            }
 
-            //ViewBag.Total = "Total de lanches: ";
-            //ViewBag.TotalLanches= totalLanches;
-
-            //return View(lanches);
-
-            var lanchesListViewModel = new LancheListViewModel();
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
-
-            var totalLanches = lanchesListViewModel.Lanches.Count();
-            ViewBag.Total = "Total de lanches: ";
-            ViewBag.TotalLanches = totalLanches;
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
 
             return View(lanchesListViewModel);
-
-
-
-
         }
     }
 }
